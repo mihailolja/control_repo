9e24038b98d363c78d4cd66dafa327bf344f3e97
mihailolja <55547431+mihailolja@users.noreply.github.com>
2019-09-23 16:06:07 +0100

    Update agent_nodes.pp

--- site/profile/manifests/agent_nodes.pp
+++ site/profile/manifests/agent_nodes.pp
@@ -2,4 +2,6 @@ class profile::agent_nodes {
   include dockeragent
   dockeragent::node {'web.puppet.vm':}
   dockeragent::node {'db.puppet.vm':}
+  dockeragent::node {'zzz.puppet.vm':}
+  dockeragent::node {'a1.puppet.vm':}
   }
